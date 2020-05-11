835fe6313455a1e5f32a94cb13f84efeced29009
AngelinaKorsun <31704835+AngelinaKorsun@users.noreply.github.com>
2020-05-11 14:26:12 +0300

    Додаткове завдання

--- an_korr_rts3/src/utils/fermats/factorization.js
+++ an_korr_rts3/src/utils/fermats/factorization.js
@@ -2,11 +2,16 @@ import {isSquare, smallerEq} from '../mathFunctions';
 
 const factorizationFermats = n => {
   if (n === 1) return [{x: 1, y: 1}, ''];
+  const begin = Date.now();
   for (let x = Math.ceil(Math.sqrt(n)); smallerEq(x, n); x += 1) {
     const ySquared = x ** 2 - n;
     if (isSquare(ySquared)) {
       const y = Math.sqrt(ySquared);
-      return [{x: Number(x - y), y: Number(x + y)}, ''];
+      const end = Date.now();
+      return [
+        {time: `${end - begin} msec`, x: Number(x - y), y: Number(x + y)},
+        '',
+      ];
     }
   }
 };
